db3213af7379b2d9310657fa65d6f087b66d2aa8
MOONKYU401 <mmk6553@gmail.com>
2025-05-02 00:23:06 -0600

    bcrypt to bcryptjs

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./user"); 
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
